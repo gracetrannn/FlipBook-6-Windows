edcc64012b1f897778c280d477a107456e4a81c2
585 <585@happy.com>
2023-11-12 15:55:02 +0700

    convert branch to libdgc-v2

--- WinResourceLoader.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "ResourceLoader.h"
-
-class WinResourceLoader: public ResourceLoader {
-public:
-	ResourcePtr LoadImprint() override;
-	ResourcePtr LoadDefaultPalette() override;
-};
